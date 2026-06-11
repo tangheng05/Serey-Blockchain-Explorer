1ff8b3deef3ff700b3751bf76ef90372169a48e7
Draxler <76139625+tangheng05@users.noreply.github.com>
2026-06-11 09:24:16 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
   plugins: [vue()],
-  base: process.env.VITE_PUBLIC_PATH || '/',
+  base: env.VITE_PUBLIC_PATH || '/',
   build: {
     outDir: 'docs',
   },
@@ -23,4 +25,5 @@ export default defineConfig({
     },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
   },
+  }
 })
